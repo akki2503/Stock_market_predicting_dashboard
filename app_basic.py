4c83471f573b707165bee37d8155735ed8a1fc34
akki2503 <akshat.2597@gmail.com>
2020-10-18 22:36:22 +0530

    we improved a bit

--- app_basic.py
+++ app_basic.py
@@ -11,14 +11,55 @@ import dash_html_components as html
 import plotly.graph_objects as go
 import pandas as pd
 
+from tensorflow import keras
+from sklearn.preprocessing import MinMaxScaler
 
 # In[5]:
-df = pd.read_csv('INFY.NS.csv', index_col=0, parse_dates=True)
+def read_stock_data(stock_name):
+    stock_data = pd.read_csv(stock_name+'.csv')
+    return stock_data
+
+def process_data_for_prediction(raw_data, model):
+    n_lag = 5
+    raw_data = raw_data.drop(['Adj Close'], axis=1)
+    for i in range(1,n_lag+1):
+        var_name = 'Shifted Close Price ' + str(i)
+        raw_data[var_name] = raw_data['Close'].shift(i)
+    raw_data = raw_data.dropna()
+
+    training_set = raw_data.values
+    training_set.shape
+
+    sc_x = MinMaxScaler(feature_range = (0, 1))
+    training_set_x_scaled = sc_x.fit_transform(training_set[:,1:11])
+    sc_y = MinMaxScaler(feature_range = (0, 1))
+    training_set_y_scaled = sc_y.fit_transform(training_set[:,-1:])
+
+    train_sub_x = training_set_x_scaled[0:4500]
+    train_sub_y = training_set_y_scaled[0:4500]
+    test_sub_x = training_set_x_scaled[4500:]
+    test_sub_y = training_set_y_scaled[4500:]
+
+    x_train_cnn = train_sub_x.reshape(train_sub_x.shape[0],train_sub_x.shape[1],1)
+    y_train_cnn = train_sub_y
+
+    x_test_cnn = test_sub_x.reshape(test_sub_x.shape[0],test_sub_x.shape[1],1)
+    y_test_cnn = test_sub_y
+
+    y_pred = model.predict(x_test_cnn)
+    y_pred = sc_y.inverse_transform(y_pred)
+
+    return y_pred
+
+
+def load_model():
+    global model
+    model = keras.models.load_model('cnn_model')
 
 import plotly.express as px
 
 # In[18]:
-mgr_options = list(df.columns)
+mgr_options = ['Infosys']
 
 app = dash.Dash()
 app.layout = html.Div([
@@ -31,7 +72,7 @@ app.layout = html.Div([
                     'label': i,
                     'value': i,
                 } for i in mgr_options],
-                value='Close'),
+                value='Infosys'),
         ],
         style={'width': '25%',
                'display': 'inline-block'}),
@@ -43,12 +84,16 @@ app.layout = html.Div([
     dash.dependencies.Output('funnel-graph', 'figure'),
     [dash.dependencies.Input('Stock', 'value')])
 def update_graph(Stock):
-    if Stock == "Close":
-        df_plot = df.copy()
-    else:
-        df_plot = df.copy()
+    # if Stock == "Close":
+    #     df_plot = df.copy()
+    # else:
+    #     df_plot = df.copy()
+    df_plot = read_stock_data(Stock)
+    plot_values = df_plot['Close']
+
+    precessed_data = process_data_for_prediction(df_plot, model)
     
-    fig = go.Scatter(x=df_plot.index, y=df[Stock])
+    fig = go.Scatter(x=df_plot.index, y=plot_values.values)
     # fig = go.figure(px.line(df_plot, x=df_plot.index, y=df[Stock]))
 
     return {
@@ -57,4 +102,5 @@ def update_graph(Stock):
             go.Layout(title='Stock Close Price')
     }
 if __name__ == '__main__':
+    load_model()
     app.run_server(debug=True)
